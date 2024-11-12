c9081961686e7f62c795cd12fd8cd960a56e3ae6
kartikm7 <108652656+kartikm7@users.noreply.github.com>
2024-11-13 00:42:18 +0530

    chore: version patch

--- cli/index.ts
+++ cli/index.ts
@@ -22,7 +22,7 @@ async function welcome(): Promise<void> {
 program
   .name('easelog')
   .description('CLI to copy easelog for easier customization!')
-  .version('1.0.2')
+  .version('1.0.3')
 
 program
   .command('init [destination]')
